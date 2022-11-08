fe6fd8039699ef879a28ee1f7a9a56f9025f240e
bigmommys <95093190+bigmommys@users.noreply.github.com>
2022-11-09 01:06:37 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'beautyapp';
+
+  public clients: any;
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    const url = `http://127.0.0.1:5000/clients`; 
+    this.http.get(url).subscribe(value => {
+      this.clients = value;
+      console.log(value);
+    });
+  }
+
 }
